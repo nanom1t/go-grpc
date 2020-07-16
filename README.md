9fdfa55d145932c2c79d5bd61de82b537b142f99
nanom1t <nanom1t@mail.ru>
2020-07-16 14:38:38 +0300

    Calculating distance

--- README.md
+++ README.md
@@ -11,3 +11,18 @@ go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
 ```
 protoc -I location/ location/location.proto --go_out=plugins=grpc:location
 ```
+
+3. Run gRPC server:
+```
+$ go run server.go
+2020/07/16 14:36:08 Starting gRPC server...
+2020/07/16 14:36:27 Received distance GRPC request:
+2020/07/16 14:36:27 -- Point 1: latitude:50.450001 longitude:30.523333
+2020/07/16 14:36:27 -- Point 2: latitude:49.842957 longitude:24.031111
+```
+
+4. Run gRPC client:
+```
+$ go run client.go
+GRPC response: 467357.25793234253
+```
--- location/location.go
+++ location/location.go
@@ -3,6 +3,7 @@ package location
 import (
 	"log"
 	"golang.org/x/net/context"
+	"math"
 )
 
 type Server struct {
@@ -14,7 +15,29 @@ func (server *Server) Distance(context context.Context, request *DistanceRequest
 	log.Printf("-- Point 1: %s", request.Point1)
 	log.Printf("-- Point 2: %s", request.Point2)
 
-	var distance float64 = 25
+	distance := Distance(request.Point1.Latitude, request.Point1.Longitude, request.Point2.Latitude, request.Point2.Longitude)
 
 	return &DistanceResponse{Distance: distance}, nil
+}
+
+// Distance function returns the distance (in meters) between two points of
+// a given longitude and latitude relatively accurately (using a spherical
+// approximation of the Earth) through the Haversin Distance Formula for
+// great arc distance on a sphere with accuracy for small distances
+//
+// http://en.wikipedia.org/wiki/Haversine_formula
+func Distance(lat1, lon1, lat2, lon2 float64) float64 {
+	const R float64 = 6371000 // Earth radius
+
+	// convert to radians
+	var f1 = lat1 * math.Pi / 180
+	var f2 = lat2 * math.Pi / 180
+	var l1 = lon1 * math.Pi / 180
+	var l2 = lon2 * math.Pi / 180
+
+	// calculate
+	h := math.Pow(math.Sin((f2 - f1) / 2), 2) + math.Cos(f1) * math.Cos(f2) * math.Pow(math.Sin((l2 - l1) / 2), 2)
+	d := 2 * R * math.Asin(math.Sqrt(h))
+
+	return d
 }
